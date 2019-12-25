7f28a67a6f3067849c49fc9686f6efb922b9de29
Itay Pahima <itayp@harmon.ie>
2019-12-25 10:29:41 +0200

    fix state

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,7 +5,7 @@ let quizData = require('./quiz_data.json');
 class Quiz extends Component {
     constructor(props) {
         super(props);
-        this.state = {quiz_position: 0};
+        this.state = {quiz_position: 1};
     }
 
     render() {
@@ -13,7 +13,7 @@ class Quiz extends Component {
         return (
             <div>
                 <div className="QuizQuestion">
-                    { quizData["quiz_questions"][quiz_position]["instruction_text"] }
+                    { quizData["quiz_questions"][0]["instruction_text"] }
                 </div>
             </div>
         )
