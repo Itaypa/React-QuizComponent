a8fa5322bbc085fa0b2a6a2a7b2073e7068b17dd
Itay Pahima <itayp@harmon.ie>
2019-12-25 10:28:19 +0200

    another fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-    constructor() {
+    constructor(props) {
         super();
         this.state = {quiz_position: 0};
     }
